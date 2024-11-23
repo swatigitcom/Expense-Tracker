9e28765fbf19079f047f275a39c301e4531cfd08
Swati Dubey <132255205+swatigitcom@users.noreply.github.com>
2024-11-23 20:17:58 +0530

    Add files via upload
    "Initial upload of Expense Tracker project."

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to My React App</h1>
+      <p>Start building your app here!</p>
+    </div>
+  );
+}
+
+export default App;
