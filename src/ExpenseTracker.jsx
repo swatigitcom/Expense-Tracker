f33d9fe4fa181301e37ed1f213da9842f38b7505
swati dubey <swatidubey2629@gmail.com>
2024-12-06 11:01:29 +0530

    Initial commit: Add expense tracker project

---
+++ src/ExpenseTracker.jsx
@@ -0,0 +1,70 @@
+import React, { useState, useEffect } from "react";
+import './ExpenseTracker.css';
+
+
+function ExpenseTracker() {
+  const [input, setInput] = useState("");
+  const [amount, setAmount] = useState("");
+  const [expenses, setExpenses] = useState([]);
+
+  useEffect(() => {
+    const storedExpenses = JSON.parse(localStorage.getItem("expenses"));
+    if (storedExpenses) {
+      setExpenses(storedExpenses);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("expenses", JSON.stringify(expenses));
+  }, [expenses]);
+
+  const addExpense = () => {
+    if (!input || !amount) return;
+
+    const newExpense = {
+      id: expenses.length + 1,
+      title: input,
+      amount: amount,
+    };
+    setExpenses([...expenses, newExpense]);
+    setInput("");
+    setAmount("");
+  };
+
+  const deleteExpense = (id) => {
+    setExpenses(expenses.filter((expenses) => expenses.id !== id));
+  };
+
+  return (
+    <div>
+      <h2>Expense Tracker</h2>
+      <div>
+        <input
+          type="text"
+          placeholder="Expense"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <input
+          type="number"
+          placeholder="Amount"
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)}
+        />
+        <button onClick={addExpense}>Add Expense</button>
+      </div>
+      <ul className="expense-list">
+        {expenses.map((expense) => (
+          <li key={expense.id}>
+            <span>{expense.title}</span>
+            <span>{expense.amount}</span>
+
+            <button onClick={() => deleteExpense(expense.id)}> Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default ExpenseTracker;
